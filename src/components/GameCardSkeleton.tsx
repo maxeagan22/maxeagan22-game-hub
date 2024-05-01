0acc6a3f32157ad55e7cf537ab408afa88aa6304
Max Eagan <meagan7654@gmail.com>
2024-05-01 18:13:30 -0500

    Update

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -2,7 +2,7 @@ import { Card, CardBody, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 
 const GameCardSkeleton = () => {
   return (
-    <Card width="250px" borderRadius={10} overflow="hidden">
+    <Card>
       <SkeletonCircle height="200px" />
       <CardBody>
         <SkeletonText />
