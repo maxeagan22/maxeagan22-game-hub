18f47ab038b3a302cd2bfc81ccb385a6a6f79a41
Max Eagan <meagan7654@gmail.com>
2024-05-03 19:18:41 -0500

    Added SortSelector
    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -21,7 +21,10 @@ const useGames = (gameQuery : GameQuery) =>
 useData<Game>('/games', {
   params: {
     genres: gameQuery.genre?.id,
-    platforms: gameQuery.platform?.id }},
+    platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
+  }
+},
     
   //Dependencies
   [gameQuery]);
