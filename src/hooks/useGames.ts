95952295d8fd77af77afe4b0cb5add5b105b04ba
Max Eagan <meagan7654@gmail.com>
2024-07-24 21:03:44 -0500

    refator - simplify time calculations

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,7 @@
-import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import ms from "ms";
 import { GameQuery } from "../App";
-import  APIClient, { FetchResponse } from "../services/api-client";
+import APIClient, { FetchResponse } from "../services/api-client";
 import { Platform } from "./usePlatforms";
 
 const apiClient = new APIClient<Game>('/games')
@@ -32,7 +33,7 @@ useInfiniteQuery<FetchResponse<Game>, Error>({
         getNextPageParam: (lastPage, allPages) => {
           return lastPage.next ? allPages.length + 1 : undefined;
         },
-        staleTime: 24 * 60 * 60 * 1000 // 24 hrs
+        staleTime: ms('24h')
 
 })
 
