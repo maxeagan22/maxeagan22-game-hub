d49f1e7895b00418bc7f2c499f04a29ed32d38d0
Max Eagan <meagan7654@gmail.com>
2024-08-18 19:19:05 -0500

    fix issue

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -9,7 +9,7 @@ export interface FetchResponse<T> {
 const axiosInstance = axios.create({
   baseURL: 'https://api.rawg.io/api',
   params: {
-    key: 'df9caa0cb5a34b1fb386572e57c4f86b',
+    key: import.meta.env.VITE_API_KEY,
   },
 });
 
